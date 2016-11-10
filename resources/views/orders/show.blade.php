bb05a4ac61c4833beb8c546ea3c92d556ca54a48
Jota Vares <joe@eternallynocturnal.com.com>
2016-11-09 21:04:35 -0500

    unhides notes

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -4,27 +4,24 @@
 @section('content')
 
 	<order-table :order="{{$order}}"></order-table>
-	
-	@if(count($order->notes) > 0)
-		<div class="col-xs-12 col-md-6" style="padding-top: 15px;">
-			<h3>
-				Notes
-				<button class="btn btn-xs btn-primary" data-toggle="collapse" data-target="#addNote">ADD</button>
-			</h3>
-			<div class="well collapse" style="background-color: #fff" id="addNote">
-				<form action="{{route('order.notes.store', $order->id)}}" method="POST">
-					<input type="hidden" name="_token" value="{{csrf_token()}}">
-					<input class="form-control" placeholder="Subject" name="subject">
-					<textarea class="form-control" placeholder="Note" name="message"></textarea>
-					<button class="btn btn-xs btn-success pull-right">SAVE</button>
-				</form>
-			</div>
-			<div class="well" style="background-color: #fff">
-				@foreach($order->notes()->orderBy('updated_at', 'asc')->get() as $note)
-					<note-block note-id="{{$note->id}}"></note-block> 
-				@endforeach
-			</div>
-		@endif
+	<div class="col-xs-12 col-md-6" style="padding-top: 15px;">
+		<h3>
+			Notes
+			<button class="btn btn-xs btn-primary" data-toggle="collapse" data-target="#addNote">ADD</button>
+		</h3>
+		<div class="well collapse" style="background-color: #fff" id="addNote">
+			<form action="{{route('order.notes.store', $order->id)}}" method="POST">
+				<input type="hidden" name="_token" value="{{csrf_token()}}">
+				<input class="form-control" placeholder="Subject" name="subject">
+				<textarea class="form-control" placeholder="Note" name="message"></textarea>
+				<button class="btn btn-xs btn-success pull-right">SAVE</button>
+			</form>
+		</div>
+		<div class="well" style="background-color: #fff">
+			@foreach($order->notes()->orderBy('updated_at', 'asc')->get() as $note)
+				<note-block note-id="{{$note->id}}"></note-block> 
+			@endforeach
+		</div>
 	</div>
 
 
